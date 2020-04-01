626fc1f0a3d14b3816e245d3cf24cd548a8320e4
thomas Goudan <35572887+thomasGoudan@users.noreply.github.com>
2020-04-01 20:42:04 +0800

    博客系统

---
+++ src/test/java/wu/xiu/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package wu.xiu.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
